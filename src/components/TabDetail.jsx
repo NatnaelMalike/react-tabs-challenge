95b69cee971e8edbe3f8d3cdc0e7fcda989481ac
Natnael Malike <natnaelmalike@gmail.com>
2024-10-26 14:26:38 +0300

    style: add new styles

---
+++ src/components/TabDetail.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TabDetail = ({ url }) => {
+  return (
+    <div className="tab-container">
+    <div className="tab-detail">
+      <h1 className="header">Title 1</h1>
+      <p className="line-height">
+        'Non aliquip fugiat velit ad officia Lorem tempor cillum incididunt elit
+        proident mollit. Reprehenderit qui nisi ut occaecat minim velit deserunt
+        occaecat quis magna mollit. Veniam proident consectetur sunt mollit est
+        magna Lorem voluptate enim cupidatat consequat. Et pariatur aliquip
+        commodo nisi deserunt exercitation enim officia voluptate in nisi. Eu ea
+        esse qui est ea pariatur nostrud non elit irure. Ad exercitation Lorem
+        exercitation ipsum eiusmod ea duis ad mollit veniam aliquip veniam.
+        Lorem pariatur elit ea duis.'
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default TabDetail;
